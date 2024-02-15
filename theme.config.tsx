396b2f50f1fbe9d9ac1c381575f6086f3b7bb50c
M. Zakiyuddin Munziri <belummikir@gmail.com>
2024-02-15 14:28:14 +0800

    init

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
-  project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
-  },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
-  footer: {
-    text: 'Nextra Docs Template',
-  },
-}
+	logo: <span>Data Pemilu 2024</span>,
+	// project: {
+	// 	link: "https://github.com/shuding/nextra-docs-template",
+	// },
+	// chat: {
+	// 	link: "https://discord.com",
+	// },
+	docsRepositoryBase: "https://github.com/shuding/nextra-docs-template",
+	footer: {
+		text: "Nextra Docs Template",
+	},
+};
 
-export default config
+export default config;

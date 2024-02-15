374eefcf2138b46488d2483d50726dc77bc20054
M. Zakiyuddin Munziri <belummikir@gmail.com>
2024-02-15 14:40:03 +0800

    Update footer text in theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -11,7 +11,7 @@ const config: DocsThemeConfig = {
 	// },
 	// docsRepositoryBase: "https://github.com/shuding/nextra-docs-template",
 	footer: {
-		text: "Nextra Docs Template",
+		text: "Data Pemilu 2024",
 	},
 	head: <title>Data Pemilu 2024</title>,
 };

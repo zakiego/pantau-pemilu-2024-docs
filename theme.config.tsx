dfd40c4defd36fd9b370ba0e43f0c7d19ceb4e48
M. Zakiyuddin Munziri <belummikir@gmail.com>
2024-02-18 21:16:09 +0800

    Update project and docs repository links

--- theme.config.tsx
+++ theme.config.tsx
@@ -3,13 +3,13 @@ import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
 	logo: <span>Data Pemilu 2024</span>,
-	// project: {
-	// 	link: "https://github.com/shuding/nextra-docs-template",
-	// },
+	project: {
+		link: "https://github.com/zakiego/pantau-pemilu-2024-docs",
+	},
 	// chat: {
 	// 	link: "https://discord.com",
 	// },
-	// docsRepositoryBase: "https://github.com/shuding/nextra-docs-template",
+	docsRepositoryBase: "https://github.com/zakiego/pantau-pemilu-2024-docs",
 	footer: {
 		text: "Data Pemilu 2024",
 	},

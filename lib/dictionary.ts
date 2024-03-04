8051636aee42be01713812e9d2d59d8f966acd55
M. Zakiyuddin Munziri <belummikir@gmail.com>
2024-03-05 01:17:38 +0800

    refactor: use dictionary

---
+++ lib/dictionary.ts
@@ -0,0 +1,60 @@
+export const DICTIONARY = {
+	suara_sah: "Jumlah suara sah",
+	suara_tidak_sah: "Jumlah suara tidak sah",
+	suara_total: "Jumlah suara total",
+
+	pemilih_dpt_j: "Jumlah pemilih DPT jumlah",
+	pemilih_dpt_l: "Jumlah pemilih DPT jenis kelamin laki-laki",
+	pemilih_dpt_p: "Jumlah pemilih DPT jenis kelamin perempuan",
+	pengguna_dpt_j: "Jumlah pengguna DPT jumlah",
+	pengguna_dpt_l: "Jumlah pengguna DPT jenis kelamin laki-laki",
+	pengguna_dpt_p: "Jumlah pengguna DPT jenis kelamin perempuan",
+	pengguna_dptb_j: "Jumlah pengguna DPTb jumlah",
+	pengguna_dptb_l: "Jumlah pengguna DPTb jenis kelamin laki-laki",
+	pengguna_dptb_p: "Jumlah pengguna DPTb jenis kelamin perempuan",
+	pengguna_total_j: "Jumlah pengguna total jumlah",
+	pengguna_total_l: "Jumlah pengguna total jenis kelamin laki-laki",
+	pengguna_total_p: "Jumlah pengguna total jenis kelamin perempuan",
+	pengguna_non_dpt_j: "Jumlah pengguna non-DPT jumlah",
+	pengguna_non_dpt_l: "Jumlah pengguna non-DPT jenis kelamin laki-laki",
+	pengguna_non_dpt_p: "Jumlah pengguna non-DPT jenis kelamin perempuan",
+
+	status_suara: "Status Suara",
+	status_adm: "Status Administrasi",
+
+	tps: "Kode unik TPS, mengacu pada kolom `kode` pada tabel wilayah, dengan tingkat 5",
+	provinsi_kode:
+		"Kode unik Provinsi, mengacu pada kolom `kode` pada tabel wilayah, dengan tingkat 1",
+	provinsi_nama: "Nama provinsi",
+	kabupaten_kota_kode:
+		"Kode unik kabupaten/kota, mengacu pada kolom `kode` pada tabel wilayah, dengan tingkat 2",
+	kabupaten_kota_nama: "Nama kabupaten/kota",
+	kecamatan_kode:
+		"Kode unik kecamatan, mengacu pada kolom `kode` pada tabel wilayah, dengan tingkat 3",
+	kecamatan_nama: "Nama kecamatan",
+	kelurahan_desa_kode:
+		"Kode unik kelurahan/desa, mengacu pada kolom `kode` pada tabel wilayah, dengan tingkat 4",
+	kelurahan_desa_nama: "Nama kelurahan/desa",
+
+	psu: "Status PSU: Reguler/Pemungutan Suara Ulang/Penghitungan Suara Ulang/Pemungutan Suara Susulan/Pemungutan Suara Lanjutan",
+	ts: "Waktu kapan terakhir data diupdate oleh KPU",
+
+	url_page:
+		"Kolom tambahan untuk mengetahui URL halaman web KPU yang menampilkan data",
+	url_api:
+		"Kolom tambahan untuk mengetahui URL API yang digunakan untuk mengambil data",
+
+	updated_at:
+		"Kolom tambahan untuk menandai kapan tanggal terakhir data discrape",
+	created_at: "Kolom tambahan untuk menandai tanggal pertama data discrape",
+	fetch_count: "Kolom tambahan untuk menandai berapa kali data discrape",
+
+	suara_paslon_1: "Jumlah suara paslon 01",
+	suara_paslon_2: "Jumlah suara paslon 02",
+	suara_paslon_3: "Jumlah suara paslon 03",
+
+	persen_suara_masuk: "Persentase suara masuk",
+
+	ppwp_persen: "-",
+	ppwp_status_progress: "-",
+};

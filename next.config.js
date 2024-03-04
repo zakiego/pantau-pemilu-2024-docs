6f3db9d5116f0ada0fc2f202bc44dbdfadd5a2db
M. Zakiyuddin Munziri <belummikir@gmail.com>
2024-03-05 02:02:06 +0800

    refactor: next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
-const withNextra = require('nextra')({
-  theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.tsx',
-})
+const withNextra = require("nextra")({
+	theme: "nextra-theme-docs",
+	themeConfig: "./theme.config.tsx",
+});
 
-module.exports = withNextra()
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+module.exports = withNextra(nextConfig);
